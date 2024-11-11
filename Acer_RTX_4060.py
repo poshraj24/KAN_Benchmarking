1135b05ac84686595996b83f1dd55b9553975cf6
poshraj24 <dahal.poshraj24@gmail.com>
2024-11-11 11:25:58 +0100

    python files for process

---
+++ Acer_RTX_4060.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
+from typing import Callable, Tuple, List
+import time
+import wandb
+import json
+from datetime import datetime
+import os
